179259a8dfe8cdd031cce219e566c26b8a116b1a
D3BaNaNa <videep.mannava@gmail.com>
2024-07-15 16:21:32 -0700

    Added grid.js

---
+++ grid.js
@@ -0,0 +1,65 @@
+class Grid {
+
+    constructor() {
+
+    }
+
+    makeGrid(gridWidth, gridHeight, tileWidth, tileHeight) {
+        // initializes the grid
+
+
+    }
+
+    displayGrid(color = [0, 0, 0]) {
+        // visually displays the grid
+
+
+    }
+
+    getClicked() {
+        // returns which square of the grid was clicked on
+
+
+    }
+
+    changeTile(x, y) {
+        // changes a tile's appearance
+
+
+    }
+
+
+
+
+}
+
+
+
+class Sudoku extends Grid {
+
+    constructor() {
+
+    }
+
+
+    generate(difficulty) {
+
+
+
+    }
+
+    solve() {
+
+
+
+    }
+
+    hint() {
+
+
+
+
+    }
+
+
+}
